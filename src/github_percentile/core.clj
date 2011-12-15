8364858a7b7fa6704a446cc491313ac14228cb47
Phil Hagelberg <technomancy@gmail.com>
2011-12-15 14:57:07 -0800

    Memoize members.

--- src/github_percentile/core.clj
+++ src/github_percentile/core.clj
@@ -4,13 +4,16 @@
   (:require [tentacles.orgs :as orgs]
             [tentacles.users :as users]))
 
+(def members (memoize (fn members [org] (orgs/members org))))
+
 (defn percentile
   "If you were a Github employee, what percentile would your user ID be?"
-  [username]
-  (let [id (:id (users/user username))
-        hubbers (orgs/members "github")
-        older (filter (partial > id) (map :id hubbers))]
-    (int (* 100 (/ (count older) (count hubbers))))))
+  ([username org]
+     (let [id (:id (users/user username))
+           hubbers (members org)
+           older (filter (partial > id) (map :id hubbers))]
+       (int (* 100 (/ (count older) (count hubbers))))))
+  ([username] (percentile username "github")))
 
 (defroutes main-routes
   (GET "/:who" [who]
